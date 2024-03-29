5a7dccf1afeeafeca6a05a86229daaf96779a8ed
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2024-03-29 13:11:39 +0200

    i3 dotfiles

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+    feh --bg-scale --zoom fill ~/Pictures/OuterWilds.jpg 
+  done
+else
+  polybar --reload example &
+fi
