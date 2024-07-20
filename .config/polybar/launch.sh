d40ad64f17029f4b4991596c2c95e9a6e130bcfc
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2024-07-20 11:06:26 +0300

    further changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
+# If all your bars have ipc enabled, you can use
+# polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
-# killall -q polybar
+killall -q polybar
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload example &
-    feh --bg-scale --zoom fill ~/Pictures/OuterWilds.jpg 
   done
 else
   polybar --reload example &
