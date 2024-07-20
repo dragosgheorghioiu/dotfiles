138e0b5fa0c1028f137378a47c995745b27c9764
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2024-07-20 11:13:53 +0300

    awesomewm config

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+function run {
+    if ! pgrep $1 ; then
+        $@&
+    fi
+}
+
+if xrandr --query | grep "HDMI-0 connected"; then
+	run ~/.screenlayout/default.sh
+else
+	run ~/.screenlayout/onlyinternal.sh
+fi
+
+run picom
